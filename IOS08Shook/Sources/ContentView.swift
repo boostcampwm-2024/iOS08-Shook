13dc8b430c8e8e4c48f4d956f0b27b36396b6a77
hyunjuntyler <hyunjun1994@hanyang.ac.kr>
2024-11-12 15:28:11 +0900

    style swiftformat 적용

--- IOS08Shook/Sources/ContentView.swift
+++ IOS08Shook/Sources/ContentView.swift
@@ -5,9 +5,9 @@ import SwiftUI
 public struct ContentView: View {
     public init() {}
 
-        public var body: some View {
-            Text("Hello, World!")
-                .padding()
+    public var body: some View {
+        Text("Hello, World!")
+            .padding()
     }
 }
 
