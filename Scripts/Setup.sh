7300cff06651072db9db79c894da40249a0a3c1a
Hamp <yongbeomkwak@gmail.com>
2024-11-12 11:48:08 +0900

    chore #2 setup 스크립트 정의

---
+++ Scripts/Setup.sh
@@ -0,0 +1,33 @@
+# brew 확인
+if test -d "/opt/homebrew/bin/"; then
+    PATH="/opt/homebrew/bin/:${PATH}"
+fi
+
+if which swiftlint > /dev/null; then
+    echo "✅ swiftlint 설치 확인"
+else
+    echo "❌ swiftlint 설치 필요, swiftlint를 설치합나다."
+    brew install swiftlint
+fi
+
+if  which swiftformat > /dev/null; then
+    echo "✅ swiftformat 설치 확인"
+else
+    echo "❌ swiftformat 설치 필요, swiftformat을 설치합나다."
+    brew install swiftformat
+fi
+
+if which tuist > /dev/null; then
+    echo "✅ tuist 설치 확인"
+else
+    echo "❌ tuist 설치 필요 tuist를 설치합나다."
+
+    curl https://mise.run | sh
+    echo 'eval "$(~/.local/bin/mise activate --shims zsh)"' >> ~/.zshrc
+    source ~/.zshrc
+
+    mise install tuist
+    tuist version
+fi
+
+echo "✅ 개발 환경 기본 세팅 완료"
