65aed7b2c26024f7e0adaeaf3cdd3e279cd72970
Hamp <yongbeomkwak@gmail.com>
2024-11-12 16:04:56 +0900

    chore #11 템플릿 플러그인 정의

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -2,6 +2,7 @@ import ProjectDescription
 
 let config = Config(
     plugins: [    
+		.local(path: .relativeToRoot("Plugin/TemplatePlugin")),    
 		.local(path: .relativeToRoot("Plugin/ConfigurationPlugin")),
     ],
     generationOptions: .options()
