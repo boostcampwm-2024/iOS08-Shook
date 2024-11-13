a3ec674d7ef511046a37f15b6e90f8f394715f9d
Hamp <yongbeomkwak@gmail.com>
2024-11-13 19:20:56 +0900

    feat #38 URLQueryEncoder 구현

---
+++ Projects/Modules/NetworkModule/Sources/Request/Encoding/URLQueryEncoder.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+public struct URLQueryEncoder: RequestDataEncodable {
+    
+#warning("배열 query value는 추후 구현")
+    func encode(request: inout URLRequest, with parameters: Parameters) throws {
+        #warning("return을 에러로 교체")
+        guard let url = request.url else { return }
+        
+        if var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false), !parameters.isEmpty {
+            urlComponents.queryItems = parameters.map {
+                URLQueryItem(name: $0.key, value: "\($0.value)".urlQueryAllowed)
+            }
+            request.url = urlComponents.url
+        }
+    }
+}
+
+private extension String {
+    var urlQueryAllowed: String? {
+        self.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+    }
+}
--- Projects/Modules/NetworkModule/Tests/NetworkModuleTest.swift
+++ Projects/Modules/NetworkModule/Tests/NetworkModuleTest.swift
@@ -1,11 +1,17 @@
 import XCTest
+@testable import NetworkModule
 
 final class NetworkModuleTests: XCTestCase {
     override func setUpWithError() throws {}
 
     override func tearDownWithError() throws {}
 
-    func testExample() {
-        XCTAssertEqual(1, 1)
+    func test_query_encoder() throws {
+        var request = URLRequest(url: URL(string: "https://example.com")!)
+        let encoder = URLQueryEncoder()
+        
+        try encoder.encode(request: &request, with: ["key1": "value1 ! #!@$**", "key2": 123, "key3": true])
+        
+        XCTAssertEqual("\(request.url?.absoluteString)", "")
     }
 }
