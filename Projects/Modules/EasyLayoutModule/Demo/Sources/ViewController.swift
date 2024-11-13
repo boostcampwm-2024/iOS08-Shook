24967eaccc307f8de73df6880f116fff3451c349
Siwon-L <111dltldnjs@gmail.com>
2024-11-13 17:56:16 +0900

    chore: DemoViewController 생성

---
+++ Projects/Modules/EasyLayoutModule/Demo/Sources/ViewController.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+import EasyLayoutModule
+
+final class EasyLayoutDemoViewController: UIViewController {
+    
+    private let emptyView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .red
+        view.frame.origin = .init(x: 0, y: 0)
+        return view
+    }()
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupAttribute()
+        setupSubViewsConstraints()
+    }
+    
+    private func setupAttribute() {}
+    
+    private func setupSubViewsConstraints() {
+        view.addSubview(emptyView)
+        
+        emptyView.ezl.makeConstraint {
+            $0.height(100).width(100)
+        }
+    }
+}
