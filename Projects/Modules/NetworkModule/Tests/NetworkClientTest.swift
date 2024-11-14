4ce16747006c91988c7f1ab4ad7764084a3989d2
Hamp <yongbeomkwak@gmail.com>
2024-11-15 00:09:02 +0900

    test #68 로깅 테스트

--- Projects/Modules/NetworkModule/Tests/NetworkClientTest.swift
+++ Projects/Modules/NetworkModule/Tests/NetworkClientTest.swift
@@ -11,7 +11,7 @@ final class NetworkClientTest: XCTestCase {
     // MARK: - Success
     func test_success_response() async throws {
         session = MockURLSession(data: mockData, response: mockSuccessResponse)
-        client = NetworkClient(session: session)
+        client = NetworkClient(session: session, interceptors: [DefaultLoggingInterceptor()])
         let mockEndpoint = MockEndpoint.fetch
         
         let response = try await client.request(mockEndpoint)
@@ -24,7 +24,7 @@ final class NetworkClientTest: XCTestCase {
     // MARK: - BadGateway
     func test_bad_gateway_response() async throws {
         session = MockURLSession(data: mockData, response: mockBadGatewayResponse)
-        client = NetworkClient(session: session)
+        client = NetworkClient(session: session, interceptors: [DefaultLoggingInterceptor()] )
         let mockEndpoint = MockEndpoint.fetch
         
         var result: HTTPError?
@@ -41,7 +41,7 @@ final class NetworkClientTest: XCTestCase {
     // MARK: - BadRequest
     func test_bad_request_response() async throws {
         session = MockURLSession(data: mockData, response: mockBadRequestResponse)
-        client = NetworkClient(session: session)
+        client = NetworkClient(session: session, interceptors: [DefaultLoggingInterceptor()])
         let mockEndpoint = MockEndpoint.fetch
         
         var result: HTTPError?
@@ -53,5 +53,18 @@ final class NetworkClientTest: XCTestCase {
         
         let expectation = HTTPError.badRequest
         XCTAssertEqual(result, expectation)
+    }
+    
+    func test_query_and_body_withParams() async throws {
+        session = MockURLSession(data: mockData, response: mockSuccessResponse)
+        client = NetworkClient(session: session, interceptors: [DefaultLoggingInterceptor()])
+        let mockEndpoint = MockEndpoint.getwithParameters(queryParams: ["sort": "asc"], bodyParams: ["age": 1])
+        
+        let response = try await client.request(mockEndpoint)
+        guard let httpResponse = response.response as? HTTPURLResponse else { return XCTFail("HTTP 응답이 아닙니다.") }
+        
+        XCTAssertEqual(httpResponse.statusCode, 200)
+        XCTAssertEqual(response.data, mockData)
+        
     }
 }
