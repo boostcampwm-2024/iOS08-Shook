3f25dbfbf0a21c43d6de17d6372a0cb187680ff3
Hamp <yongbeomkwak@gmail.com>
2024-11-14 23:46:56 +0900

    feat #68 client에 interceptor 적용

--- Projects/Modules/NetworkModule/Sources/Client/NetworkClient.swift
+++ Projects/Modules/NetworkModule/Sources/Client/NetworkClient.swift
@@ -4,14 +4,16 @@ import NetworkModuleInterface
 
 final class NetworkClient<E: Endpoint>: Requestable {
     private let session: URLSessionProtocol
+    private var interceptors: [any Interceptor]
     
-    init(session: URLSessionProtocol = URLSession.shared) {
+    init(session: URLSessionProtocol = URLSession.shared, interceptors: [any Interceptor] = [] ) {
         self.session = session
+        self.interceptors = interceptors
     }
     
     func request(_ endpoint: E) async throws -> Response {
-        let request = try configureURLRequest(from: endpoint)
-        
+        var request = try configureURLRequest(from: endpoint)
+        request = try interceptRequest(with: request, from: endpoint)
         return try await requestNetworkTask(with: request, from: endpoint)
     }
 }
@@ -43,5 +45,22 @@ private extension NetworkClient {
         }
     
         return Response(request: request, data: data, response: response)
+    }
+    
+    func interceptRequest(with request: URLRequest, from endpoint: E) throws -> URLRequest {
+        var requst = request
+        
+        for interceptor in self.interceptors {
+            try interceptor.willRequest(requst, from: endpoint)
+            requst = try interceptor.prepare(requst, from: endpoint)
+        }
+        
+        return requst
+    }
+    
+    func interceptResponse(with response: Response, from endpoint: E) throws {
+        for interceptor in self.interceptors {
+            try interceptor.didRecieve(response, from: endpoint)
+        }
     }
 }
--- Projects/Modules/NetworkModule/Sources/Interceptor/Interceptor.swift
+++ Projects/Modules/NetworkModule/Sources/Interceptor/Interceptor.swift
@@ -3,7 +3,7 @@ import Foundation
 public protocol Interceptor {
     /// request를 보내기 전, 수정할 작업이 있을 경우 ex) token
     func prepare(_ request: URLRequest, from endpoint: Endpoint) throws -> URLRequest
-    /// 보내기 직전 최종 request
+    /// 전달받은 requst
     func willRequest(_ request: URLRequest, from endpoint: Endpoint) throws
     /// response 받았을 때
     func didRecieve(_ result: Response, from endpoint: Endpoint) throws
