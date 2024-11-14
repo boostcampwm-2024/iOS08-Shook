f0c9726924767a9358d406935febece3461c2ac9
hyunjuntyler <hyunjun1994@hanyang.ac.kr>
2024-11-14 11:04:17 +0900

    style #48 코드 스타일 변경

--- Projects/Modules/NetworkModule/Sources/Request/RequestError.swift
+++ Projects/Modules/NetworkModule/Sources/Request/RequestError.swift
@@ -8,7 +8,8 @@ enum RequestError: Error, LocalizedError {
         switch self {
         case .missingURL:
             return "요청에서 유효한 URL을 찾을 수 없습니다."
-        case .jsonEncodingFailed(let error):
+            
+        case let .jsonEncodingFailed(error):
             return "JSON 인코딩에 실패했습니다: \(error.localizedDescription)"
         }
     }
