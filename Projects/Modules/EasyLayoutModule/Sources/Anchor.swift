e64f787414b5e2e0cc0eb5a5f7c80f88bafd51f9
Siwon-L <111dltldnjs@gmail.com>
2024-11-16 18:52:29 +0900

    refactor #74 Anchor enum -> struct로 변경

--- Projects/Modules/EasyLayoutModule/Sources/Anchor.swift
+++ Projects/Modules/EasyLayoutModule/Sources/Anchor.swift
@@ -1,27 +1,51 @@
 import UIKit
 
 // MARK: YAnchor
-public enum YAnchor {
-    case top(Anchorable)
-    case bottom(Anchorable)
-    
-    var standard: NSLayoutYAxisAnchor {
-        switch self {
-        case let .top(view): view.topAnchor
-        case let .bottom(view): view.bottomAnchor
+public struct YAnchor {
+    enum `Type` {
+        case top(Anchorable)
+        case bottom(Anchorable)
+        
+        var standard: NSLayoutYAxisAnchor {
+            switch self {
+            case let .top(view): view.topAnchor
+            case let .bottom(view): view.bottomAnchor
+            }
         }
+    }
+    
+    let type: `Type`
+    
+    static func top(_ view: Anchorable) -> Self {
+        YAnchor(type: .top(view))
+    }
+    
+    static func bottom(_ view: Anchorable) -> Self {
+        YAnchor(type: .bottom(view))
     }
 }
 
 // MARK: XAnchor
-public enum XAnchor {
-    case leading(Anchorable)
-    case trailing(Anchorable)
-    
-    var standard: NSLayoutXAxisAnchor {
-        switch self {
-        case let .leading(view): view.leadingAnchor
-        case let .trailing(view): view.trailingAnchor
+public struct XAnchor {
+    enum `Type` {
+        case leading(Anchorable)
+        case trailing(Anchorable)
+        
+        var standard: NSLayoutXAxisAnchor {
+            switch self {
+            case let .leading(view): view.leadingAnchor
+            case let .trailing(view): view.trailingAnchor
+            }
         }
+    }
+    
+    let type: `Type`
+    
+    static func leading(_ view: Anchorable) -> Self {
+        XAnchor(type: .leading(view))
+    }
+    
+    static func trailing(_ view: Anchorable) -> Self {
+        XAnchor(type: .trailing(view))
     }
 }
