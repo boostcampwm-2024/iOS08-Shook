e30ff65a8e239928fa530c383ad8d8ad90e7db9a
hyunjuntyler <hyunjun1994@hanyang.ac.kr>
2024-11-18 16:21:02 +0900

    feat #82 뷰모델 프로토콜 추가 및 적용

--- Projects/Features/BaseFeature/Sources/BaseViewController.swift
+++ Projects/Features/BaseFeature/Sources/BaseViewController.swift
@@ -2,21 +2,33 @@ import UIKit
 
 import BaseFeatureInterface
 
-open class BaseViewController: UIViewController {
+open class BaseViewController<VM: ViewModel>: UIViewController, ViewLifeCycle {
+    var viewModel: VM
+    
+    public init(viewModel: VM) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required public init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     open override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
         setupLayouts()
         setupActions()
+        setupBind()
     }
     
     open override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         setupStyles()
     }
-}
-
-extension BaseViewController: ViewLifeCycle {
+    
+    open func setupBind() { }
+    
     open func setupViews() { }
     
     open func setupStyles() { }
