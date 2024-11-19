ccf991e2ebb2286c8a94ba2b89b705162597ccf0
Siwon-L <111dltldnjs@gmail.com>
2024-11-20 01:19:16 +0900

    feat #101 채팅을 입력할 ChatInputField 구현

---
+++ Projects/Features/LiveStreamFeature/Sources/Chating/Views/ChatInputField.swift
@@ -0,0 +1,73 @@
+import UIKit
+
+import BaseFeature
+import DesignSystem
+import EasyLayoutModule
+
+final class ChatInputField: BaseView {
+    private let heartButton = UIButton()
+    private let inputField = UITextView()
+    private let sendButton = UIButton()
+    
+    private let clipView = UIView()
+    
+    override func setupViews() {
+        addSubview(heartButton)
+        addSubview(clipView)
+        
+        clipView.addSubview(inputField)
+        clipView.addSubview(sendButton)
+        
+        heartButton.setImage(DesignSystemAsset.Image.heart24.image, for: .normal)
+        heartButton.setContentHuggingPriority(.required, for: .horizontal)
+        
+        inputField.isScrollEnabled = false
+        inputField.textContainerInset = .zero
+
+        sendButton.setContentHuggingPriority(.required, for: .horizontal)
+        sendButton.setImage(DesignSystemAsset.Image.send24.image, for: .normal)
+        sendButton.setContentCompressionResistancePriority(.required, for: .horizontal)
+    }
+    
+    override func setupStyles() {
+        backgroundColor = .gray
+        
+        clipView.backgroundColor = .clear
+        clipView.layer.cornerRadius = 20
+        clipView.clipsToBounds = true
+        clipView.layer.borderWidth = 1
+        clipView.layer.borderColor = UIColor.white.cgColor
+        
+        inputField.font = .setFont(.body2())
+        inputField.backgroundColor = .clear
+        inputField.textColor = .white
+    }
+    
+    override func setupLayouts() {
+        heartButton.ezl.makeConstraint {
+            $0.leading(to: self, offset: 20)
+                .bottom(to: self, offset: -16)
+        }
+        
+        clipView.ezl.makeConstraint {
+            $0.vertical(to: self, padding: 10)
+                .leading(to: heartButton.ezl.trailing, offset: 10)
+                .trailing(to: self, offset: -20)
+        }
+        
+        NSLayoutConstraint.activate([
+            clipView.heightAnchor.constraint(greaterThanOrEqualToConstant: 40)
+        ])
+        
+        inputField.ezl.makeConstraint {
+            $0.vertical(to: clipView, padding: 10)
+                .leading(to: clipView, offset: 16)
+                .trailing(to: sendButton.ezl.leading, offset: -12)
+        }
+        
+        sendButton.ezl.makeConstraint {
+            $0.trailing(to: clipView, offset: -15)
+                .bottom(to: clipView, offset: -8)
+        }
+    }
+}
