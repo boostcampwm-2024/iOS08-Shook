78f960ce35b70513c9cc5d2429ac0f71dfa41bd6
Hyunjun <hyunjuntyler@gmail.com>
2024-11-20 04:34:01 +0900

    design #89 셀 디자인 수정

--- Projects/Features/MainFeature/Sources/SmallCollectionViewCell.swift
+++ Projects/Features/MainFeature/Sources/SmallCollectionViewCell.swift
@@ -8,39 +8,55 @@ final class SmallCollectionViewCell: BaseCollectionViewCell {
     static var identifier: String { String(describing: Self.self) }
 
     private let thumbnail = UIImageView()
-    private let stack = UIStackView()
+    private let descriptionStack = UIStackView()
     
     private let title = UILabel()
     private let subtitle1 = UILabel()
     private let subtitle2 = UILabel()
     
+    private let liveCircle = UIView()
+    
     override func setupViews() {
         contentView.addSubview(thumbnail)
-        contentView.addSubview(stack)
+        contentView.addSubview(liveCircle)
+        contentView.addSubview(descriptionStack)
         
-        stack.addArrangedSubview(title)
-        stack.addArrangedSubview(subtitle1)
-        stack.addArrangedSubview(subtitle2)
+        descriptionStack.addArrangedSubview(title)
+        descriptionStack.addArrangedSubview(subtitle1)
+        descriptionStack.addArrangedSubview(subtitle2)
     }
     
     override func setupLayouts() {
         thumbnail.ezl.makeConstraint {
             $0.leading(to: contentView)
-                .width(164)
-                .height(103)
+                .width(contentView.frame.width * 0.45)
+                .height(contentView.frame.width * 0.45 * 0.5625)
         }
         
-        stack.axis = .vertical
-        stack.spacing = 4
-        stack.ezl.makeConstraint {
+        descriptionStack.ezl.makeConstraint {
             $0.leading(to: thumbnail.ezl.trailing, offset: 8)
+                .trailing(to: contentView)
                 .centerY(to: thumbnail)
+        }
+        
+        liveCircle.ezl.makeConstraint {
+            $0.top(to: thumbnail, offset: 12)
+                .leading(to: thumbnail, offset: 12)
+                .size(with: 8)
         }
     }
     
     override func setupStyles() {
         thumbnail.contentMode = .scaleAspectFill
         thumbnail.clipsToBounds = true
+        thumbnail.layer.cornerRadius = 8
+        
+        liveCircle.backgroundColor = DesignSystemAsset.Color.mainGreen.color
+        liveCircle.layer.cornerRadius = 4
+        liveCircle.clipsToBounds = true
+        
+        descriptionStack.axis = .vertical
+        descriptionStack.spacing = 4
         
         title.font = .setFont(.body2())
         title.numberOfLines = 2
