bb0469fb7460620230d409798b5c567a5c20c05f
Hamp <yongbeomkwak@gmail.com>
2024-11-20 23:38:21 +0900

    chore #116 코드 정리

--- Projects/Features/LiveStreamFeature/Sources/Player/Views/PlayerControlView.swift
+++ Projects/Features/LiveStreamFeature/Sources/Player/Views/PlayerControlView.swift
@@ -8,17 +8,45 @@ import DesignSystem
 public protocol PlayerControlViewAction {
     var playButtonDidTap: AnyPublisher<Void, Never> { get }
     var sliderValueDidChange: AnyPublisher<Double, Never> { get }
+    var expandButtonDidTap: AnyPublisher<Void, Never> { get }
+}
+
+private enum ImageConstants {
+    case play
+    case pause
+    case zoomIn
+    case zoomOut
+    
+    var image: UIImage {
+        switch self {
+
+        case .zoomIn:
+            return DesignSystemAsset.Image.zoomIn24.image
+            
+        case .zoomOut:
+            return DesignSystemAsset.Image.zoomOut48.image
+            
+        case .play:
+            return DesignSystemAsset.Image.play48.image
+            
+        case .pause:
+            return DesignSystemAsset.Image.pause48.image
+        }
+    }
 }
 
 final class PlayerControlView: BaseView {
     private let playButton: UIButton = UIButton()
+    private let expandButton: UIButton = UIButton()
     var timeControlView: TimeControlView = TimeControlView()
     
     @Published private var playButtonTapPublisher: Void = ()
     @Published private var sliderValuePublisher: Double = .zero
+    @Published private var expandButtonTapPublisher: Void = ()
     
     override func setupViews() {
         self.addSubview(playButton)
+        self.addSubview(expandButton)
         self.addSubview(timeControlView)
     }
     
@@ -32,14 +60,24 @@ final class PlayerControlView: BaseView {
                 .horizontal(to: self, padding: 15)
                 .bottom(to: self, offset: -20)
         }
+        
+        expandButton.ezl.makeConstraint {
+            $0.trailing(to: self, offset: -20)
+                .top(to: self, offset: 15)
+        }
+        
     }
     
     override func setupStyles() {
         self.backgroundColor = .black.withAlphaComponent(0.5)
         
         var playButtonConfig = UIButton.Configuration.plain()
-        playButtonConfig.image = DesignSystemAsset.Image.play48.image
+        playButtonConfig.image = ImageConstants.play.image
         playButton.configuration = playButtonConfig
+        
+        var expandButtonConfig = UIButton.Configuration.plain()
+        expandButtonConfig.image = ImageConstants.zoomIn.image
+        expandButton.configuration = expandButtonConfig
     }
     
     override func setupActions() {
@@ -51,10 +89,19 @@ final class PlayerControlView: BaseView {
         timeControlView.value
             .compactMap { Double($0)}
             .assign(to: &$sliderValuePublisher)
+        
+        expandButton.addAction(UIAction { [weak self] _ in
+            guard let self else { return }
+            self.expandButtonTapPublisher = ()
+        }, for: .touchUpInside)
     }
 }
 
-extension PlayerControlView {    
+extension PlayerControlView {
+    private func toggleExpandButtonImage(_ expanded: Bool) {
+        expandButton.configuration?.image = expanded ? ImageConstants.zoomOut.image : ImageConstants.zoomIn.image
+    }
+    
     public func togglePlayerButtonAnimation(_ status: AVPlayer.TimeControlStatus) {
         playButton.transform = CGAffineTransform(scaleX: .zero, y: .zero)
         
@@ -64,16 +111,21 @@ extension PlayerControlView {
                        initialSpringVelocity: 0.4,
                        options: .allowUserInteraction) {
             if status == .playing {
-                self.playButton.configuration?.image = DesignSystemAsset.Image.pause48.image
+                self.playButton.configuration?.image = ImageConstants.pause.image
             } else {
-                self.playButton.configuration?.image = DesignSystemAsset.Image.play48.image
+                self.playButton.configuration?.image = ImageConstants.play.image
             }
             self.playButton.transform  = .identity
         }
     }
+
 }
 
 extension PlayerControlView: PlayerControlViewAction {
+    var expandButtonDidTap: AnyPublisher<Void, Never> {
+        $expandButtonTapPublisher.eraseToAnyPublisher()
+    }
+    
     var playButtonDidTap: AnyPublisher<Void, Never> {
         $playButtonTapPublisher.eraseToAnyPublisher()
     }
