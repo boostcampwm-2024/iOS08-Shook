c19a4ae3cf682b6a03be41b0abc462085147566a
Siwon-L <111dltldnjs@gmail.com>
2024-11-21 11:14:32 +0900

    chore #121 ChatingViewModel 생성

---
+++ Projects/Features/LiveStreamFeature/Sources/Chating/ChatingViewModel.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+import BaseFeatureInterface
+
+public final class ChatingViewModel: ViewModel {
+    public struct Input {
+        
+    }
+    
+    public struct Output {
+        
+    }
+    
+    public init() {
+        
+    }
+    
+    public func transform(input: Input) -> Output {
+        return Output()
+    }
+}
