63bffd1c8fd204e4b37224648d26e18c049a246e
In Ye, Kim <ide7270@naver.com>
2024-11-24 12:19:02 +0900

    feat #148 방송 시작, 종료 시 UserDefaults에 값 변경

--- Projects/App/BroadcastUploadExtension/Sources/SampleHandler.swift
+++ Projects/App/BroadcastUploadExtension/Sources/SampleHandler.swift
@@ -1,6 +1,18 @@
 import ReplayKit
 
 class SampleHandler: RPBroadcastSampleHandler {
-    override func broadcastStarted(withSetupInfo setupInfo: [String : NSObject]?) {
+    private let sharedDefaults = UserDefaults(suiteName: "group.kr.codesquad.boostcamp9.Shook")!
+    private let isStreamingKey = "isStreaming"
+    
+    override func broadcastStarted(withSetupInfo setupInfo: [String: NSObject]?) {
+        self.sharedDefaults.set(true, forKey: self.isStreamingKey)
+        
+    }
+    
+    override func broadcastFinished() {
+        sharedDefaults.set(false, forKey: isStreamingKey)
+    }
+    
+    override func processSampleBuffer(_ sampleBuffer: CMSampleBuffer, with sampleBufferType: RPSampleBufferType) {
     }
 }
