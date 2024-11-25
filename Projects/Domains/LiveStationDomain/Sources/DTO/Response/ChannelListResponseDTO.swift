78cb280f6d02caae7110fefd6307cf77b6771f1f
Siwon-L <111dltldnjs@gmail.com>
2024-11-25 20:44:23 +0900

    feat #186 ChannelListResponseDTO 구현

--- Projects/Domains/LiveStationDomain/Sources/DTO/Response/ChannelListResponseDTO.swift
+++ Projects/Domains/LiveStationDomain/Sources/DTO/Response/ChannelListResponseDTO.swift
@@ -3,15 +3,5 @@ import Foundation
 import LiveStationDomainInterface
 
 public struct ChannelListResponseDTO: Decodable {
-    let content: [ChannelResponse]
-}
-
-public struct ChannelResponse: Decodable {
-    let channelId: String
-}
-
-extension ChannelResponse {
-    public func toDomain() -> ChannelEntity {
-        ChannelEntity(id: self.channelId, name: self.channelId)
-    }
+    let content: [ChannelResponseDTO]
 }
