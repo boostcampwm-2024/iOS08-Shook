c3b91d1e036ff2d7557f71af373678b8fa836129
In Ye, Kim <ide7270@naver.com>
2024-11-25 18:51:25 +0900

    feat #183 프로토콜에 메소드 추가(인터페이스)

--- Projects/Domains/LiveStationDomain/Interface/Repository/LiveStationRepository.swift
+++ Projects/Domains/LiveStationDomain/Interface/Repository/LiveStationRepository.swift
@@ -2,4 +2,5 @@ import Combine
 
 public protocol LiveStationRepository {
     func fetchChannelList() -> AnyPublisher<[ChannelEntity], Error>
+    func fetchThumbnail(channelId: String) -> AnyPublisher<[String], any Error>
 }
