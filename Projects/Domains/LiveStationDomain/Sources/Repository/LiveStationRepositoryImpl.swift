b011a9af5579ad2641b0b29f0abe1ecf01564373
In Ye, Kim <ide7270@naver.com>
2024-11-25 22:47:53 +0900

    chore Edit fetchChannelList Lint Error

--- Projects/Domains/LiveStationDomain/Sources/Repository/LiveStationRepositoryImpl.swift
+++ Projects/Domains/LiveStationDomain/Sources/Repository/LiveStationRepositoryImpl.swift
@@ -6,7 +6,7 @@ import LiveStationDomainInterface
 public final class LiveStationRepositoryImpl: BaseRepository<LiveStationEndpoint>, LiveStationRepository {
     public func fetchChannelList() -> AnyPublisher<[ChannelEntity], any Error> {
         return request(.fetchChannelList, type: ChannelListResponseDTO.self)
-            .map{ $0.content.map{ $0.toDomain() } }
+            .map { $0.content.map { $0.toDomain() }}
             .eraseToAnyPublisher()
     }
     
