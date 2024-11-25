15e48feb4ca9e52a40e5d7276549ec7d03ca9164
In Ye, Kim <ide7270@naver.com>
2024-11-25 16:31:23 +0900

    chore #175 keykind 추가

--- Projects/Domains/LiveStationDomain/Sources/Util/Secrets.swift
+++ Projects/Domains/LiveStationDomain/Sources/Util/Secrets.swift
@@ -3,6 +3,9 @@ import Foundation
 enum KeyKind: String {
     case secretKey = "SECRET_KEY"
     case accessKey = "ACCESS_KEY"
+    case cdnDomain = "CDN_DOMAIN"
+    case profileID = "PROFILE_ID"
+    case cdnInstanceNo = "CDN_INSTANCE_NO"
 }
 
 func config(key: KeyKind) -> String {
