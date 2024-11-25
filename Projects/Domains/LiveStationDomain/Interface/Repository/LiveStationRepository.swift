ec6bc81fa3b72c307ed271105f9767278ca4e41d
Siwon-L <111dltldnjs@gmail.com>
2024-11-25 20:55:32 +0900

    feat #188  Live Station Repository 채널 삭제 인터페이스 구현

--- Projects/Domains/LiveStationDomain/Interface/Repository/LiveStationRepository.swift
+++ Projects/Domains/LiveStationDomain/Interface/Repository/LiveStationRepository.swift
@@ -4,4 +4,5 @@ public protocol LiveStationRepository {
     func fetchChannelList() -> AnyPublisher<[ChannelEntity], Error>
     func fetchThumbnail(channelId: String) -> AnyPublisher<[String], any Error>
     func createChannel(name: String) -> AnyPublisher<ChannelEntity, any Error>
+    func deleteChannel(id: String) -> AnyPublisher<ChannelEntity, any Error>
 }
