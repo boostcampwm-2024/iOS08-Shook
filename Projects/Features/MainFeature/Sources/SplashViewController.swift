f70520cf0803c0fb986ba3d94e66a775ba3fc27e
Hamp <yongbeomkwak@gmail.com>
2024-11-26 16:21:03 +0900

    chore #202 로고 이미지 추가

---
+++ Projects/Features/MainFeature/Sources/SplashViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SplashViewController.swift
+//  MainFeature
+//
+//  Created by yongbeomkwak on 11/26/24.
+//  Copyright © 2024 kr.codesquad.boostcamp9. All rights reserved.
+//
+
+import Foundation
