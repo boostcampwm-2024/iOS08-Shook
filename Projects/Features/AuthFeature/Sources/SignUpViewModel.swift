ec5b4d8bf0efed21a3f2b14114670873337f752c
hyunjuntyler <hyunjun1994@hanyang.ac.kr>
2024-11-26 21:26:32 +0900

    feat #212 닉네임 유효성 검사 기능 추가

--- Projects/Features/AuthFeature/Sources/SignUpViewModel.swift
+++ Projects/Features/AuthFeature/Sources/SignUpViewModel.swift
@@ -1,10 +1,33 @@
+import Combine
+
 import BaseFeatureInterface
 
 public class SignUpViewModel: ViewModel {
-    public struct Input { }
-    public struct Output { }
+    public struct Input {
+        let didWriteUserName: PassthroughSubject<String, Never> = .init()
+    }
+    public struct Output {
+        let isValidate: PassthroughSubject<Bool, Never> = .init()
+    }
     
-    public func transform(input: Input) -> Output { Output() }
+    private let output = Output()
+    private var cancellables = Set<AnyCancellable>()
+    
+    public func transform(input: Input) -> Output {
+        input.didWriteUserName
+            .sink { [weak self] name in
+                if let isValidate = self?.validate(with: name) {
+                    self?.output.isValidate.send(isValidate)
+                }
+            }
+            .store(in: &cancellables)
+        
+        return output
+    }
     
     public init() { }
+    
+    private func validate(with name: String) -> Bool {
+        name.count >= 2 && name.count <= 10 && name.allSatisfy { $0.isLetter || $0.isNumber }
+    }
 }
