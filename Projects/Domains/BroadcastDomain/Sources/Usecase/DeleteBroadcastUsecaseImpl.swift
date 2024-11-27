0c3b4cd162accc67f148de33885486340ac365b6
Hyunjun <hyunjuntyler@gmail.com>
2024-11-27 22:47:02 +0900

    feat #227 Usecase 구현

---
+++ Projects/Domains/BroadcastDomain/Sources/Usecase/DeleteBroadcastUsecaseImpl.swift
@@ -0,0 +1,16 @@
+import Combine
+import Foundation
+
+import BroadcastDomainInterface
+
+public struct DeleteBroadcastUsecaseImpl: DeleteBroadcastUsecase {
+    private let repository: any BroadcastRepository
+    
+    public init(repository: any BroadcastRepository) {
+        self.repository = repository
+    }
+    
+    public func execute(id: String) -> AnyPublisher<Void, Error> {
+        repository.deleteBroadcast(id: id)
+    }
+}
