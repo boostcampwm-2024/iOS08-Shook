420b0ee290e205a6d091e23ad4057aeb8e428ecd
Siwon-L <111dltldnjs@gmail.com>
2024-11-27 15:05:47 +0900

    feat #215 최근 채팅으로 이동 버튼 구현

--- Projects/Features/LiveStreamFeature/Sources/Chating/Views/ChatingListView.swift
+++ Projects/Features/LiveStreamFeature/Sources/Chating/Views/ChatingListView.swift
@@ -1,8 +1,8 @@
 import Combine
 import UIKit
 
-import DesignSystem
 import BaseFeature
+import DesignSystem
 import EasyLayoutModule
 
 protocol ChatInputFieldAction {
@@ -15,6 +15,13 @@ final class ChattingListView: BaseView {
     private let chatEmptyView = ChatEmptyView()
     private let chatInputField = ChatInputField()
     private let recentChatButton = UIButton()
+    
+    @Published private var isScrollFixed = true
+    
+    private var recentChatButtonShowConstraints: [NSLayoutConstraint] = []
+    private var recentChatButtonHideConstraints: [NSLayoutConstraint] = []
+    
+    private var subscription = Set<AnyCancellable>()
 
     private lazy var dataSource = UITableViewDiffableDataSource<Int, ChatInfo>(
         tableView: chatListView
@@ -48,6 +55,7 @@ final class ChattingListView: BaseView {
         
         titleLabel.text = "실시간 채팅"
         
+        chatListView.delegate = self
         chatListView.register(ChattingCell.self, forCellReuseIdentifier: ChattingCell.identifier)
         chatListView.register(SystemAlarmCell.self, forCellReuseIdentifier: SystemAlarmCell.identifier)
         chatListView.backgroundView = chatEmptyView
@@ -58,7 +66,7 @@ final class ChattingListView: BaseView {
         titleLabel.font = .setFont(.body1())
         
         chatListView.backgroundColor = .clear
-        chatListView.keyboardDismissMode = .interactive
+        chatListView.keyboardDismissMode = .onDrag
         chatListView.allowsSelection = false
         chatListView.separatorStyle = .none
         
@@ -86,17 +94,65 @@ final class ChattingListView: BaseView {
                 .bottom(to: self)
         }
         
-        recentChatButton.ezl.makeConstraint {
-            $0.centerX(to: self)
-                .bottom(to: chatInputField.ezl.top, offset: -8)
-        }
+        recentChatButton.translatesAutoresizingMaskIntoConstraints = false
+        recentChatButtonShowConstraints = [
+            recentChatButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            recentChatButton.bottomAnchor.constraint(equalTo: chatInputField.topAnchor, constant: -8)
+        ]
+        recentChatButtonHideConstraints = [
+            recentChatButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            recentChatButton.bottomAnchor.constraint(equalTo: chatInputField.bottomAnchor, constant: 0)
+        ]
+        NSLayoutConstraint.activate(recentChatButtonHideConstraints)
+    }
+    
+    override func setupActions() {
+        $isScrollFixed
+            .debounce(for: .milliseconds(50), scheduler: DispatchQueue.main)
+            .sink { [weak self] in
+                self?.updateRecentChatButtonConstraint(isHidden: $0)
+            }
+            .store(in: &subscription)
+        
+        recentChatButton.addAction(
+            UIAction { [weak self] _ in
+                self?.updateRecentChatButtonConstraint(isHidden: true)
+                self?.scrollToBottom()
+            },
+            for: .touchUpInside
+        )
+        
+        sendButtonDidTap
+            .sink { [weak self] _ in
+                self?.isScrollFixed = true
+            }
+            .store(in: &subscription)
+    }
+    
+    private func lastIndexPath() -> IndexPath? {
+        let lastRowIndex = chatListView.numberOfRows(inSection: 0) - 1
+        guard lastRowIndex >= 0 else { return nil }
+        return IndexPath(row: lastRowIndex, section: 0)
     }
     
     private func scrollToBottom() {
         let lastRowIndex = chatListView.numberOfRows(inSection: 0) - 1
-        guard lastRowIndex >= 0 else { return }
-        let indexPath = IndexPath(row: lastRowIndex, section: 0)
+        guard lastRowIndex >= 0,
+              let indexPath = lastIndexPath() else { return }
         chatListView.scrollToRow(at: indexPath, at: .bottom, animated: true)
+    }
+    
+    private func updateRecentChatButtonConstraint(isHidden: Bool) {
+        UIView.animate(withDuration: 0.2) {
+            if isHidden {
+                NSLayoutConstraint.activate(self.recentChatButtonHideConstraints)
+                NSLayoutConstraint.deactivate(self.recentChatButtonShowConstraints)
+            } else {
+                NSLayoutConstraint.activate(self.recentChatButtonShowConstraints)
+                NSLayoutConstraint.deactivate(self.recentChatButtonHideConstraints)
+            }
+            self.layoutIfNeeded()
+        }
     }
 }
 
@@ -108,12 +164,22 @@ extension ChattingListView {
         snapshot.appendSections([0])
         snapshot.appendItems(chatList)
         self.dataSource.apply(snapshot, animatingDifferences: false)
-        scrollToBottom()
+        if isScrollFixed {
+            scrollToBottom()
+        }
     }
 }
 
 extension ChattingListView: ChatInputFieldAction {
     var sendButtonDidTap: AnyPublisher<ChatInfo?, Never> {
         chatInputField.$sendButtonDidTapPublisher.dropFirst().eraseToAnyPublisher()
+    }
+}
+
+extension ChattingListView: UITableViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard let lastIndexPath = lastIndexPath(),
+              let indexPathList = chatListView.indexPathsForVisibleRows else { return }
+        isScrollFixed = indexPathList.contains(lastIndexPath)
     }
 }
