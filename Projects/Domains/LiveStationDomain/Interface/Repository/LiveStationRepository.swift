d89d4fad7d446781ba7429b98c9830d54b06e5f1
In Ye, Kim <ide7270@naver.com>
2024-11-27 13:23:27 +0900

    feat #230 usecase 구현

--- Projects/Domains/LiveStationDomain/Interface/Repository/LiveStationRepository.swift
+++ Projects/Domains/LiveStationDomain/Interface/Repository/LiveStationRepository.swift
@@ -3,7 +3,7 @@ import Combine
 public protocol LiveStationRepository {
     func fetchChannelList() -> AnyPublisher<[ChannelEntity], Error>
     func fetchThumbnail(channelId: String) -> AnyPublisher<String, any Error>
-    func receiveBroadcast(channelId: String) -> AnyPublisher<[BroadcastEntity], any Error>
+    func fetchBroadcast(channelId: String) -> AnyPublisher<[VideoEntity], any Error>
     func createChannel(name: String) -> AnyPublisher<ChannelEntity, any Error>
     func deleteChannel(id: String) -> AnyPublisher<ChannelEntity, any Error>
 }
