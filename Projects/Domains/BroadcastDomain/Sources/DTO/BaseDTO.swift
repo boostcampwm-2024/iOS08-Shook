980d67e841884223d9b835160c846bd0ccd41bcd
hyunjuntyler <hyunjun1994@hanyang.ac.kr>
2024-11-28 23:01:43 +0900

    fix #249 BaseDTO 추가

---
+++ Projects/Domains/BroadcastDomain/Sources/DTO/BaseDTO.swift
@@ -0,0 +1,4 @@
+struct BaseDTO: Decodable {
+    let statusCode: Int32
+    let message: String
+}
--- Projects/Domains/BroadcastDomain/Sources/Repository/BroadcastRepositoryImpl.swift
+++ Projects/Domains/BroadcastDomain/Sources/Repository/BroadcastRepositoryImpl.swift
@@ -5,7 +5,7 @@ import BroadcastDomainInterface
 
 public final class BroadcastRepositoryImpl: BaseRepository<BroadcastEndpoint>, BroadcastRepository {
     public func makeBroadcast(id: String, title: String, owner: String, description: String) -> AnyPublisher<Void, any Error> {
-        request(.make(id: id, title: title, owner: owner, description: description), type: BroadcastDTO.self)
+        request(.make(id: id, title: title, owner: owner, description: description), type: BaseDTO.self)
             .map { _ in () }
             .eraseToAnyPublisher()
     }
@@ -17,7 +17,7 @@ public final class BroadcastRepositoryImpl: BaseRepository<BroadcastEndpoint>, B
     }
     
     public func deleteBroadcast(id: String) -> AnyPublisher<Void, any Error> {
-        request(.delete(id: id), type: BroadcastDTO.self)
+        request(.delete(id: id), type: BaseDTO.self)
             .map { _ in () }
             .eraseToAnyPublisher()
     }
